4e6e6b37f7e78ead14df24d8320301414ff5095f
Szymon Brud <szymon.brud@crodu.com>
2023-09-08 16:36:49 +0200

    Add prismic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
@@ -6,5 +8,30 @@ module.exports = {
     title: `POC-luxe`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: "gatsby-source-prismic",
+      /**
+       * @type {import("gatsby-source-prismic").PluginOptions}
+       */
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        // accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+        // routes: [
+        //   {
+        //     type: "article",
+        //     path: "/articles/:uid",
+        //   },
+        //   {
+        //     type: "page",
+        //     path: "/:uid",
+        //   },
+        // ],
+      },
+    },
+  ],
 }
